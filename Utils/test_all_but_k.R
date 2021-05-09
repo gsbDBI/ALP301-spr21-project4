a50bfac4a667bc7524e78e50af242c7431761434
JJL <jlangloi@stanford.edu>
2021-05-09 17:38:13 +0000

    fix bugs test_all_but_k

--- Utils/test_all_but_k.R
+++ Utils/test_all_but_k.R
@@ -6,14 +6,20 @@
 if (!exists("run_get_item_scores")) run_get_item_scores <- FALSE
 if (!exists("run_get_top_x_recommendations")) run_get_top_x_recommendations <- FALSE
 
-test_all_but_k<-function(k, ratings_matrix, type, params=list()){
+test_all_but_k<-function(k, ratings_matrix, type, params=list(), num_rows=NaN){
   if (run_get_item_scores) source("/cloud/project/RecSys/get_item_scores.R", local = knitr::knit_global())
   if (run_get_top_x_recommendations) source("/cloud/project/RecSys/get_top_x_recommendations.R", local = knitr::knit_global())
   precision_vector<-vector()
   recall_vector<-vector()
   rmse_vector<-vector()
   #Change the line below to run it on the entire dataset
-  num_rows<-min(50,nrow(ratings_matrix))
+  if (is.finite(num_rows)) {
+    num_rows<-min(num_rows,nrow(ratings_matrix))
+  } else {
+    num_rows<-nrow(ratings_matrix)
+  }
+  update_similarity = !(type %in% c("ibcf", "cbf"))
+  if(!update_similarity) get_item_scores <- get_item_scores_generator(ratings_matrix, type, params)
   for (userid in 1:num_rows){
     user<-ratings_matrix[userid,]
     index_of_known<-which(user!=0)
@@ -23,9 +29,8 @@ test_all_but_k<-function(k, ratings_matrix, type, params=list()){
       save_old<-user[stories_removed]
       ratings_matrix[userid,stories_removed]<-0
       
-      get_item_scores <- get_item_scores_generator(ratings_matrix, type, params)
+      if(update_similarity) get_item_scores <- get_item_scores_generator(ratings_matrix, type, params)
       recommended<-get_top_x_recommendations(userid, k, ratings_matrix, get_item_scores)
-      
       matched<-length(intersect(story_ids[stories_removed],recommended))
       precision_vector<-append(precision_vector,matched/k)
       recall_vector<-append(recall_vector,(matched/(length(index_of_known))))
