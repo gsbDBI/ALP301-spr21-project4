be9d6df7cb6725d4743d8a83dae658b121b42c6e
JJL <jlangloi@stanford.edu>
2021-05-20 23:56:23 +0000

    fix plot diversity, now it takes a raitings_matrix/utility_matrix as one of the inputs

--- Utils/plot_diversity_recsys.R
+++ Utils/plot_diversity_recsys.R
@@ -1,4 +1,4 @@
-plot_diversity_recsys<-function(filename) {
+plot_diversity_recsys<-function(filename, ratings_matrix) {
   recommender_results <- read.csv(file = paste("/cloud/project/Results/",filename, sep = ""))
   recommender_results_item_one <- recommender_results %>% filter(item_rank <= 1) %>% select(story_id) %>% unique()
   recommender_results_item_five <- recommender_results %>% filter(item_rank <= 5) %>% select(story_id) %>% unique()
@@ -6,10 +6,10 @@ plot_diversity_recsys<-function(filename) {
   index_results_one <- which(recommender_results_item_one$story_id %in% story_ids)
   index_results_five <- which(recommender_results_item_five$story_id %in% story_ids)
   
-  popularity_ones <- colSums(utility_matrix[, index_results_one]>0.3)
-  popularity_fives <- colSums(utility_matrix[, index_results_five]>0.3)
+  popularity_ones <- colSums(ratings_matrix[, index_results_one]>0.3)
+  popularity_fives <- colSums(ratings_matrix[, index_results_five]>0.3)
   
-  readers_per_story <- (colSums(utility_matrix>0.3))
+  readers_per_story <- (colSums(ratings_matrix>0.3))
   
   # Create a data frame of recommendations
   df1 <- data.frame(x = readers_per_story, label = rep("Overall", length(readers_per_story)))
