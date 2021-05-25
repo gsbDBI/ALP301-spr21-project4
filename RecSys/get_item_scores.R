3eeb80ba84ba46714f7b3d0bc1d4796d0ba8c451
johanna-eriksson <johanna.e.eriksson@gmail.com>
2021-05-25 13:10:22 -0700

    First version of ensemble ready to test

--- RecSys/get_item_scores.R
+++ RecSys/get_item_scores.R
@@ -1,7 +1,7 @@
 # Title     : Get All Item Scores
 # Objective : Get all the items score from a user
-# Created by: juan
-# Created on: 5/8/21
+# Created by: juan & johanna
+# Created on: 5/8/21, modified 5/25/21
 
 run_get_item_scores <- FALSE
 
@@ -9,6 +9,7 @@ if (!exists("run_source_ubcf")) run_source_ubcf <- TRUE
 if (!exists("run_source_ibcf")) run_source_ibcf <- TRUE
 if (!exists("run_source_svd")) run_source_svd <- TRUE
 if (!exists("run_source_cbf")) run_source_cbf <- TRUE
+if (!exists("run_source_random")) run_source_random <- TRUE
 
 path = "/cloud/project/RecSys"
 get_item_scores_generator<-function(utility_matrix, type, params=list()) {
@@ -44,7 +45,15 @@ get_item_scores_generator<-function(utility_matrix, type, params=list()) {
         cbf_get_item_scores(userid, ratings_matrix, similarity_matrix_story)
       }
     )
-  } else if(type == 'ensamble') {
+  } else if(type == 'random') {
+    if(run_source_random) source(paste(path, "/Models/random.R", sep=""), local = knitr::knit_global())
+    similarity_matrix_random <- random_get_random_matrix(utility_matrix)
+    return(
+      function(userid, ratings_matrix){
+        random_get_item_scores(userid, ratings_matrix, similarity_matrix_random)
+      }
+    )
+  } else if(type == 'ensemble') {
     if(run_source_cbf) source(paste(path, "/Models/cbf.R", sep=""), local = knitr::knit_global())
     similarity_matrix_story <- cbf_get_similarity_matrix(utility_matrix, params)
     
@@ -52,34 +61,24 @@ get_item_scores_generator<-function(utility_matrix, type, params=list()) {
     similarity_matrix_user <- ubcf_get_similarity_matrix(utility_matrix)
     
     if(run_source_ibcf) source(paste(path, "/Models/ibcf.R", sep=""), local = knitr::knit_global())
+    similarity_matrix_item <- ibcf_get_similarity_matrix(utility_matrix)
+    
     if(run_source_svd) source(paste(path, "/Models/svd.R", sep=""), local = knitr::knit_global())
+    factors <- svd_get_decomposition(utility_matrix, params_svd)
     
     return(
       function(userid, ratings_matrix) {
-        x1 = ubcf_get_item_scores(userid, ratings_matrix, similarity_matrix_user)
-        x2 = cbf_get_item_scores(userid, ratings_matrix, similarity_matrix_story)
+        x1 = cbf_get_item_scores(userid, ratings_matrix, similarity_matrix_story)
+        x2 = ubcf_get_item_scores(userid, ratings_matrix, similarity_matrix_user)
+        x3 = ibcf_get_item_scores(userid, ratings_matrix, similarity_matrix_item)
+        x4 = svd_get_item_scores(userid, factors$U, factors$Vprime)
         
-        w->params$w
+        w <- params$w
         
-        w$x1 * x1 + w$x2 * x2
+        w$x1 * x1 + w$x2 * x2 + w$x3 * x3 + w$x4 * x4
       }
     )
-  } else if(type == 'ensambel2') {
-    # params <- list(w=list(cbf=0.5, ubcf=0.5), cbf=list(story_info=story_info, story_ids=story_ids), ubcf=list())
-    cbf_recys <- get_item_scores_generator(utility_matrix, "cbf", params$cbf)
-    ubcf_recys <- get_item_scores_generator(utility_matrix, "ubcf", params$ubcf)
-    return(
-      function(userid, ratings_matrix) {
-        x_cbf = cbf_recys(userid, ratings_matrix)
-        x_ubcf = ubcf_recys(userid, ratings_matrix)
-        
-        w->params$w
-        
-        w$cbf * x_cbf + w$ubcf * x_ubcf
-      }
-    )
-  } else {
+  }
+  }else {
     return()
   }
-  
-}
