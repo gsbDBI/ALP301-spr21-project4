33f28272ab5b31453a79c84749ec950d4beea11d
Eric Zhao <eyzhao@stanford.edu>
2021-05-26 09:18:45 +0000

    Handle different value imputations

--- RecSys/get_top_x_recommendations.R
+++ RecSys/get_top_x_recommendations.R
@@ -3,13 +3,13 @@
 # Created by: juan
 # Created on: 5/8/21
 
-get_top_x_recommendations<-function(userid, X, ratings_matrix, get_item_scores) {
+get_top_x_recommendations<-function(userid, X, ratings_matrix, get_item_scores, unknown_value = 0.0) {
   item_scores <- get_item_scores(userid, ratings_matrix)
   story_ids<-as.integer(colnames(ratings_matrix))
   # We need to remove items that they already know to from our recommendations.
   user_row<-ratings_matrix[userid,]
 
-  unknown_stories<-user_row==0
+  unknown_stories<-user_row == unknown_value 
 
   names_unknown<-story_ids[unknown_stories]
   # unknown_stories_name <- names(unknown_stories[unknown_stories == TRUE])
