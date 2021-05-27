f32023157231048c13ba2a27a38d1354c370147d
johanna-eriksson <johanna.e.eriksson@gmail.com>
2021-05-26 22:14:01 -0700

    Add ensemble model

--- RecSys/get_item_scores.R
+++ RecSys/get_item_scores.R
@@ -1,7 +1,7 @@
 # Title     : Get All Item Scores
 # Objective : Get all the items score from a user
-# Created by: juan
-# Created on: 5/8/21
+# Created by: juan & johanna
+# Created on: 5/8/21, modified 5/25/21
 
 run_get_item_scores <- FALSE
 
@@ -62,7 +62,32 @@ get_item_scores_generator<-function(utility_matrix, type, params=list()) {
         random_get_item_scores(userid, ratings_matrix, similarity_matrix_random)
       }
     )
-  }else {
+  } else if(type == 'ensemble') {
+    if(run_source_cbf) source(paste(path, "/Models/cbf.R", sep=""), local = knitr::knit_global())
+    similarity_matrix_story <- cbf_get_similarity_matrix(utility_matrix, list(story_info=story_info, story_ids=story_ids)) # Made parameters explicit
+
+    if(run_source_ubcf) source(paste(path, "/Models/ubcf.R", sep=""), local = knitr::knit_global())
+    similarity_matrix_user <- ubcf_get_similarity_matrix(utility_matrix)
+
+    if(run_source_ibcf) source(paste(path, "/Models/ibcf.R", sep=""), local = knitr::knit_global())
+    similarity_matrix_item <- ibcf_get_similarity_matrix(utility_matrix)
+
+    if(run_source_svd) source(paste(path, "/Models/svd.R", sep=""), local = knitr::knit_global())
+    factors <- svd_get_decomposition(utility_matrix, d=20) # Made parameters explicit
+
+    return(
+      function(userid, ratings_matrix) {
+        x1 = cbf_get_item_scores(userid, ratings_matrix, similarity_matrix_story)
+        x2 = ubcf_get_item_scores(userid, ratings_matrix, similarity_matrix_user)
+        x3 = ibcf_get_item_scores(userid, ratings_matrix, similarity_matrix_item)
+        x4 = svd_get_item_scores(userid, factors$U, factors$Vprime)
+
+        w <- c(0.25, 0.25, 0.25, 0.25)
+
+        w[1] * x1 + w[2] * x2 + w[3] * x3 + w[4] * x4
+      }
+    )
+  } else {
     return()
   }
 }
